641ca86ae8b314031bcf0f2a28de8fb9f4df1993
Zxy <76756794@qq.com>
2020-10-11 09:41:46 +0800

    添加注释

--- HelloProjet/src/MainController.java
+++ HelloProjet/src/MainController.java
@@ -6,6 +6,7 @@ public class MainController {
     public void testHelloWrold() {
 
         System.out.println("try dev_zhangxiyan_20201010");
+        //重新添加新增内容
         System.out.println("try a wrong merge");
         System.out.println("use f_BRP_20201011");
 
